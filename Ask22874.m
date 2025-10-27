33b2afc8feebe799758995e47b29f7db9a11ec85
Chondromatidis Eleftherios <148573871+ChondromatidisEleftherios@users.noreply.github.com>
2025-10-27 16:46:30 +0200

    27/10/2025

--- Ask22874.m
+++ Ask22874.m
@@ -1,31 +1,34 @@
-function res = Ask22874(n)
-  thetikaOrismenos = 0;
+function mat = Ask22874(n)
+  thetikaOrismenos = 0; %Tha th xrhsimopoihsw san Boolean (1==true kai 0==false)
   while thetikaOrismenos == 0
       counter = 0;
   for row=1:1:n
     for col=1:1:n
-      if (row >= col)
-       symmetrikosPinakas (row, col) = rand();
+      if (row <= col)
+       symmetricMatrix (row, col) = rand();
        endif
     endfor
   endfor
   for row=1:1:n
     for col=1:1:n
-      if (row < col)
-        symmetrikosPinakas(row,col) = symmetrikosPinakas(col,row);
+      if (row > col)
+        symmetricMatrix(row,col) = symmetricMatrix(col,row);
       endif
     endfor
   endfor
     for rc=1:1:n
-    if (symmetrikosPinakas(rc,rc) > 0)
+    if (symmetricMatrix(rc,rc) > 0.0)
       counter=counter+1;
     endif
     if (counter==n)
      thetikaOrismenos = 1;
     endif
   endfor
-  disp("Currect Matrixʼ:");
-  disp(symmetrikosPinakas);
+  disp("Current Matrix:");
+  disp(symmetricMatrix);
+  disp("\n");
   endwhile
-  res = symmetrikosPinakas
-  end
+  mat = symmetricMatrix
+end
+
+%Chondromatidis Eleftherios 2874
